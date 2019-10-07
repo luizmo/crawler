0ecc465fab6e6e7dc70370ff0e7f7109978363a7
Luiz Miguel <luizmiguel.oliveira18@gmail.com>
2019-10-06 21:28:55 -0300

    WIP

---
+++ app.py
@@ -0,0 +1,43 @@
+
+import requests
+from bs4 import BeautifulSoup
+import urllib
+from flask import Flask
+from flask_restful import Resource, Api, reqparse
+
+app = Flask(__name__)
+api = Api(app)
+
+
+class SteamSearch(Resource):
+    def post(self):
+        parser = reqparse.RequestParser()
+        parser.add_argument('term', required=True,
+                            help='Um termo precisa ser passado')
+        args = parser.parse_args()
+
+        formattedSearchTerm = urllib.urlencode({'term': args.term})
+
+        r = requests.get('https://www.google.com/search?q={formattedSearchTerm}&tbm=nws')
+
+        soup = BeautifulSoup(r.text, 'html.parser')
+
+        resultsRow = soup.find_all('div')
+
+        results = resultsRow
+
+        for resultRow in resultsRow:
+            uri = resultRow.get('a', { 'class' : 'top' })
+            link = uri.get('href')
+            results.append({
+                'uri': uri,
+                'link': link
+            })
+
+        return results
+
+
+api.add_resource(SteamSearch, '/news')
+
+if __name__ == '__main__':
+    app.run(debug=True)
